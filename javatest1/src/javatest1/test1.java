85ecd2aa29a1e59da1c0dd8343cfcf7927d5261f
김미란 <@LAPTOP-333H51FO>
2025-01-20 09:01:22 +0900

    자바숙제

---
+++ javatest1/src/javatest1/test1.java
@@ -0,0 +1,21 @@
+package javatest1;
+
+public class test1 { 
+	
+	public static void main (String[] args) {
+		
+		
+		int cigarette = 3;
+		int cigarette_minute = cigarette*2*365*20;
+		int cigarette_hour = cigarette_minute/60;
+		
+		System.out.println(cigarette_minute);
+	
+		
+		System.out.println(cigarette_hour);
+		
+	
+		System.out.println(cigarette_hour/24);
+}
+
+}
